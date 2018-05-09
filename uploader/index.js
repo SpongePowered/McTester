e4efd710d58bbb102740a6b463877bcc54332efa
Aaron Hill <aa1ronham@gmail.com>
2018-05-08 21:43:06 -0400

    Upload images to imgur, and post comments

--- uploader/index.js
+++ uploader/index.js
@@ -2,16 +2,42 @@ const imgurUploader = require('imgur-uploader');
 const GitHub = require('github-api');
 const fs = require('fs');
 
-function postComment() {
-	var gh = new GitHub({token: process.env.GITHUB_TOKEN});
-	return gh.getIssues('AaronBot1011', 'GHTest').createIssueComment(1, 'This is a comment!');
+class ImageWrapper {
+    constructor(raw, title) {
+        this.raw = raw;
+        this.title = title;
+    }
+
+}
+
+function postComment(message) {
+	let gh = new GitHub({token: process.env.GITHUB_TOKEN});
+	return gh.getIssues('AaronBot1011', 'GHTest').createIssueComment(1, message).catch(function(error) {
+	    console.log("Err: " + error.response);
+    });
 }
 
 function uploadImages(images) {
-    return Promise.all(images.map(image => imgurUploader(image, {title: "McTester image"}).then(data => data.link)));
+    return Promise.all(images.map(image => imgurUploader(image.raw, {title: image.title})));
 }
 
-//result = uploadImages([fs.readFileSync('test.png')]);
-//result.then(links => console.log("Got links: " + links));
+function uploadAndComment(images) {
+    if (images.length === 0) {
+        return;
+    }
 
-postComment().then(() => console.log("Posted comment!"));
+    return uploadImages(images).then(uploads => {
+        let message = "Some Minecraft integration tests for this PR failed. The following screenshots were taken:\n";
+
+        for (let upload of uploads) {
+            message += upload.title + "\n";
+            message += "![](" + upload.link + ")";
+        }
+        return postComment(message);
+    });
+}
+
+uploadAndComment([new ImageWrapper(fs.readFileSync('test1.png'), "First title"), new ImageWrapper(fs.readFileSync('test2.png'), "Second title")]).then(() => console.log("All done!"));
+
+//result = uploadImages();
+//result.then(links => console.log("Got links: " + links));
