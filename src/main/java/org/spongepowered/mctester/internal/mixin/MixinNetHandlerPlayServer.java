e00d581414267892711b47068f982c84eb670801
Aaron Hill <aa1ronham@gmail.com>
2018-07-18 18:58:12 -0400

    Block timeouts during tests

---
+++ src/main/java/org/spongepowered/mctester/internal/mixin/MixinNetHandlerPlayServer.java
@@ -0,0 +1,25 @@
+package org.spongepowered.mctester.internal.mixin;
+
+import jdk.internal.org.objectweb.asm.Opcodes;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.util.text.ITextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(NetHandlerPlayServer.class)
+public abstract class MixinNetHandlerPlayServer {
+
+    @Redirect(method = "processKeepAlive", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetHandlerPlayServer;disconnect(Lnet/minecraft/util/text/ITextComponent;)V"))
+    private void onKeepAliveDisconnectTimeout(NetHandlerPlayServer this$0, ITextComponent reason) {
+        System.err.println("Cancelling timeout in NetHandlerPlayServer#processKeepAlive");
+        // Do nothing - we don't want any timeouts during testing
+    }
+
+    @Redirect(method = "update", at = @At(value = "FIELD", target = "Lnet/minecraft/network/NetHandlerPlayServer;field_194403_g:Z", opcode = Opcodes.GETFIELD))
+    private boolean onCheckUpdateDisconnectTimeout(NetHandlerPlayServer this$0) {
+        System.err.println("Cancelling timeout in NetHandlerPlayServer#update");
+        return false;
+    }
+
+}
---
+++ src/main/java/org/spongepowered/mctester/internal/mixin/MixinNetworkManager.java
@@ -0,0 +1,22 @@
+package org.spongepowered.mctester.internal.mixin;
+
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.util.text.TextComponentTranslation;
+import org.apache.logging.log4j.LogManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetworkManager.class)
+public abstract class MixinNetworkManager {
+
+    @Inject(method = "exceptionCaught", cancellable = true, at = @At(value = "NEW", target = "net/minecraft/util/text/TextComponentTranslation", ordinal = 0))
+    public void onTimeoutException(ChannelHandlerContext p_exceptionCaught_1_, Throwable p_exceptionCaught_2, CallbackInfo ci) {
+        System.err.println("Caught timeout exception in NetworkManager - printing out exception but keeping channel open!");
+        LogManager.getLogger().debug(new TextComponentTranslation("disconnect.timeout").getUnformattedText(), p_exceptionCaught_2);
+        ci.cancel();
+    }
+
+}
