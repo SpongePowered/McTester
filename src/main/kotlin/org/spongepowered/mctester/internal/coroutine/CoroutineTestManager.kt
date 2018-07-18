6072dde9cb47a00977dfe63bcc4ae0382740f9bf
Aaron Hill <aa1ronham@gmail.com>
2018-07-17 20:37:57 -0400

    Delete some dead code

--- src/main/kotlin/org/spongepowered/mctester/internal/coroutine/CoroutineTestManager.kt
+++ src/main/kotlin/org/spongepowered/mctester/internal/coroutine/CoroutineTestManager.kt
@@ -12,44 +12,10 @@ import kotlin.coroutines.experimental.CoroutineContext
 class CoroutineTestManager(val block: suspend CoroutineScope.() -> Unit, val testerManager: TesterManager) {
 
     var continuation: Continuation<Any>? = null
-    //private val startContinuation: Continuation<Unit> = block.createCoroutine(CallbackCompletion(this))
-    //internal var completed = false
 
     fun runToCompletion() {
         runBlocking(context = MainThreadDispatcher(this.testerManager), block = this.block)
-
-        /*this.startCoroutine()
-        while (!this.completed) {
-
-        }*/
     }
-
-    suspend fun mySuspend(myInt: Int) {
-
-    }
-
-    /*suspend fun doSuspend(block: ((Any) -> Unit)) {
-        suspendCoroutine<Any> {
-            this.continuation = it
-            block(this::driveCoroutine)
-        }
-    }
-
-    private fun startCoroutine() {
-        if (this.continuation != null) {
-            throw IllegalStateException("Already started coroutine!")
-        }
-        this.driveCoroutine(Unit)
-    }
-
-    private fun driveCoroutine(value: Any) {
-        val continuation = this.continuation
-        if (continuation == null) {
-            this.startContinuation.resume(Unit)
-        } else {
-            continuation.resume(value)
-        }
-    }*/
 }
 
 private class MainThreadDispatcher(val testManager: TesterManager): CoroutineDispatcher() {
@@ -68,10 +34,3 @@ private class MainThreadDispatcher(val testManager: TesterManager): CoroutineDis
     }
 
 }
-
-/*private class CallbackCompletion<T>(val coroutineTestManager: CoroutineTestManager): AbstractCoroutine<T>(EmptyCoroutineContext) {
-
-    override fun onCompleted(value: T) {
-        this.coroutineTestManager.completed = true
-    }
-}*/
