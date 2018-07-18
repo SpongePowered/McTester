8b65bb0e9c0cce262499a2640946278286679170
Aaron Hill <aa1ronham@gmail.com>
2018-07-18 19:51:27 -0400

    Improve timeout prevention

--- src/main/java/org/spongepowered/mctester/internal/mixin/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/mctester/internal/mixin/MixinNetHandlerPlayServer.java
@@ -4,14 +4,18 @@ import jdk.internal.org.objectweb.asm.Opcodes;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.util.text.ITextComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(NetHandlerPlayServer.class)
 public abstract class MixinNetHandlerPlayServer {
 
+    @Shadow private boolean field_194403_g;
+
     @Redirect(method = "processKeepAlive", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/NetHandlerPlayServer;disconnect(Lnet/minecraft/util/text/ITextComponent;)V"))
     private void onKeepAliveDisconnectTimeout(NetHandlerPlayServer this$0, ITextComponent reason) {
+        this.field_194403_g = false;
         System.err.println("Cancelling timeout in NetHandlerPlayServer#processKeepAlive");
         // Do nothing - we don't want any timeouts during testing
     }
