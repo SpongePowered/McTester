6d1323be4352b784d74cc76cad0e8e0d13cfb3d8
Aaron Hill <aa1ronham@gmail.com>
2018-07-18 21:31:57 -0400

    Immoral hack to make LaunchClassLoader.transformers thread-safe
    Normally, only one thread (either the client thread on the dedicated
    client, or the server thread on the dedicated server) is actively doing
    anything when the game starts up. This is probably why this issue went
    unnoticed for so long.
    
    However, our JUnit runner (along with JUnit) is actively loading classes
    on a different thread while the game is performing initializtion
    (including transformer registrations). This lead to a
    ConcurrentModificationException occuring whenever a transformer was
    registered at the same time as an off-thread classload.

--- src/main/java/org/spongepowered/mctester/api/MinecraftRunnerTweaker.java
+++ src/main/java/org/spongepowered/mctester/api/MinecraftRunnerTweaker.java
@@ -24,6 +24,7 @@
  */
 package org.spongepowered.mctester.api;
 
+import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.launchwrapper.LaunchClassLoader;
@@ -32,9 +33,29 @@ import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.mctester.internal.RemoveSecurityManager;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MinecraftRunnerTweaker implements ITweaker {
+
+    public MinecraftRunnerTweaker() {
+        // This is your fault, LaunchWrapper, not mine. You drove me to this
+        // We wait until LaunchClassLoader has started the main class before loading classes.
+        // However, transformers can still be registered in the future. To avoid any possiblility
+        // of ConcurrentModificationExceptions, we install a thread-safe List implementation.
+        try {
+            System.err.println("Hacking LaunchClassLoader to make 'transformers' thread-safe");
+            Field transformersField = LaunchClassLoader.class.getDeclaredField("transformers");
+            CopyOnWriteArrayList<IClassTransformer> newTransformers = new CopyOnWriteArrayList<>();
+            transformersField.setAccessible(true);
+            transformersField.set(Launch.classLoader, newTransformers);
+        } catch (Throwable e) {
+            System.err.println("Exception while trying to fix LaunchClassLoader!");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
@@ -64,7 +85,6 @@ public class MinecraftRunnerTweaker implements ITweaker {
 
     @Override
     public String[] getLaunchArguments() {
-        RunnerEvents.setLaunchClassLoaderFuture(Launch.classLoader);
         return new String[0];
     }
 }
--- src/main/java/org/spongepowered/mctester/internal/mixin/MixinMain.java
+++ src/main/java/org/spongepowered/mctester/internal/mixin/MixinMain.java
@@ -1,12 +1,21 @@
 package org.spongepowered.mctester.internal.mixin;
 
 import net.minecraft.client.main.Main;
+import net.minecraft.launchwrapper.Launch;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.mctester.api.RunnerEvents;
 
 @Mixin(Main.class)
 public abstract class MixinMain {
+
+    @Inject(method = "main", at = @At("HEAD"), remap = false)
+    private static void onMain(CallbackInfo ci) {
+        RunnerEvents.setLaunchClassLoaderFuture(Launch.classLoader);
+    }
 
     @Redirect(method = "main", at = @At(value = "INVOKE", target = "Ljava/lang/Runtime;addShutdownHook(Ljava/lang/Thread;)V"))
     private static void onAddShutdownHook(Runtime runtime, Thread thread) {
