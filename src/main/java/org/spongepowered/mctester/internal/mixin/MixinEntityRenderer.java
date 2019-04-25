ce1e7686ed90c5b824b183a7dd964298c3984776
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2019-04-25 11:39:12 -0500

    Add remap=false

--- src/main/java/org/spongepowered/mctester/internal/mixin/MixinEntityRenderer.java
+++ src/main/java/org/spongepowered/mctester/internal/mixin/MixinEntityRenderer.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(EntityRenderer.class)
+@Mixin(value = EntityRenderer.class, remap = false)
 public abstract class MixinEntityRenderer {
 
     /**
